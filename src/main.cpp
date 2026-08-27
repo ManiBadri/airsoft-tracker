9dbbfa683e9ff7f6f246e974cf6fa6a3d6d97525
Mani Badri <moneybadri@gmail.com>
2026-08-27 16:33:38 -0400

    tft basics and ini

--- src/main.cpp
+++ src/main.cpp
@@ -25,7 +25,13 @@ void setup() {
   pinMode(TFT_BL, OUTPUT);
   digitalWrite(TFT_BL, HIGH);      //backlight on
 
-  tft.initR(INITR_MINI160x80);
+  tft.initR(INITR_MINI160x80_PLUGIN);
+
+
+  tft.invertDisplay(false);   //fixes inverted black/white on many ST7735 clones
+  tft.setRotation(1);
+
+
   tft.setRotation(1);              //adjust 0-3 if orientation looks wrong
   tft.fillScreen(ST77XX_BLACK);
 
@@ -33,14 +39,13 @@ void setup() {
   tft.setTextColor(ST77XX_WHITE);
   tft.setTextSize(1);
   tft.println("Hello watch!");
-  Serial.println("Display initialized");
 }
 
 void loop() {
-  tft.fillRect(0, 10, 160, 10, ST77XX_BLACK); //clear just this line to avoid flicker
-  tft.setCursor(0, 10);
+  tft.drawRect(0, 0, 160, 80, ST77XX_GREEN); //border
+  tft.fillRect(5, 10, 150, 10, ST77XX_BLACK); //clear just one line 
+  tft.setCursor(5, 10);
   tft.setTextColor(ST77XX_RED);
   tft.println("Alive: " + String(millis() / 1000));
-  Serial.println("Alive...");
   delay(1000);
 }
